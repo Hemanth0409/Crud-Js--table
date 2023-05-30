737dbc7a1d450a66abbe8a906801ea3582c0db17
hemanth04/09 <hemanthbalakrishnan2002@gmail.com>
2023-05-30 10:27:35 +0530

    recent commit

--- json.js
+++ json.js
@@ -93,6 +93,7 @@ function showLogInBox() {
     Swal.fire({
         title: "Log In Page",
         showCloseButton: true,
+        closeOnClickOutside: false,
         html: '<input id="id" type="hidden">' +
             '<input id="Usermail" class="swal2-input" placeholder="User_Mail">' +
             '<input id="Password" type="password" class="swal2-input" placeholder="Password">',
@@ -140,6 +141,8 @@ function showUserCreateBox() {
     Swal.fire({
 
         title: "Add Restaurant Details ",
+        showCloseButton: true,
+        closeOnClickOutside: false,
         html: '<input id="id" type="hidden">' +
             '<input id="RestaurantName" class="swal2-input" placeholder="RestaurantName">' +
             '<select name="country" id="RestaurantType"class="swal2-input" style="width:270px"><option value="" selected disabled>-- select --</option><option value="Veg">Veg</option><option value="Non-Veg">Non Veg</option><option value="Veg/Non-veg">Veg/Non Veg</option></select>' +
@@ -198,6 +201,8 @@ function showUserEditBox(id) {
             console.log(objects);
             Swal.fire({
                 title: "Edit Restaurant-List",
+                showCloseButton: true,
+                closeOnClickOutside: false,
                 html: '<input id="id" type="hidden" value="' +
                     objects[`${id}`] + '">' +
                     '<input id="RestaurantName" class="swal2-input" placeholder="First" value="' +
@@ -269,7 +274,6 @@ function userDelete(id) {
         text: "You won't be able to revert this!",
         type: 'warning',
         icon: 'warning',
-        showCloseButton: true,
         showCancelButton: true,
         focusConfirm: false,
         confirmButtonText: '<i class="fa fa-thumbs-up"></i> Delete!',
@@ -407,186 +411,4 @@ function validate_edit() {
         })
         return true;
     }
-}
-//Food-list
-
-function loadcard_food(Food_name = "") {
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("GET", `http://localhost:3000/Food?Food_name_like=${Food_name}`);
-    xhttp.send();
-
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText);
-            var cardHTML = "";
-            const objects = JSON.parse(this.responseText);
-            for (let object of objects) {
-                cardHTML += '<div class="card text-white bg-dark col-3 mt-5">';
-                cardHTML += '<img src="' + object["Image"] + '" height="200vh"">';
-                cardHTML += '<p class="card-subtitle ">' + object["Food_name"] + "</p>";
-                cardHTML += '<a href="#" class="card-text text-white link-dark text-decoration-none">' +
-                    "Price: " +
-                    object["Price"] +
-                    "</a>";
-                cardHTML += '<button type="button" class="btn btn-secondary ms-1" onclick="showUserEdit_foodBox(' +
-                    object["id"] +
-                    ')"><i class="fa-sharp fa-solid fa-user-pen"></i></button>';
-                cardHTML += '<button type="button" class="btn  btn-danger ms-1" onclick="userDelete_food(' +
-                    object["id"] +
-                    ')"><i class="fa-sharp fa-solid fa-trash"></i></button>';
-                cardHTML += "</div>";
-            }
-            document.getElementById("mycards").innerHTML = cardHTML;
-        }
-    };
-}
-
-loadcard_food();
-
-// searching
-function searchFood() {
-    $("#content").hide();
-    const Foodname = document.getElementById("searchvalue1").value;
-    loadcard_food(Foodname);
-}
-
-function showUserCreateBox_Card() {
-    Swal.fire({
-        title: "Add New Food",
-        showCloseButton: true,
-        html: '<input id="id" type="hidden">' +
-            '<input id="Food_name" class="swal2-input" placeholder="Food_name">' +
-            '<input id="Price" class="swal2-input" placeholder="Price">' +
-            '<input id="Image" class="form-control swa4l1-input mt-4" type="file">',
-        preConfirm: () => {
-            userCreate_card();
-        },
-    });
-}
-
-function userCreate_card() {
-
-    const Food_name = document.getElementById("Food_name").value;
-    const Price = document.getElementById("Price").value;
-    const Image = document.getElementById("Image");
-    const file = "assets/images/" + Image.files[0].name;
-    console.log("file")
-    const xhttp = new XMLHttpRequest();
-
-    xhttp.open("POST", "http://localhost:3000/Food");
-    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    console.log(Food_name)
-    xhttp.send(
-        JSON.stringify({
-            Image: file,
-            Food_name: Food_name,
-            Price: Price,
-        })
-    );
-    console.log(xhttp)
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            loadcard_food();
-        }
-    };
-}
-
-function showUserEdit_foodBox(id) {
-    console.log(id);
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("GET", `http://localhost:3000/Food/${id}`);
-    xhttp.send();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            const objects = JSON.parse(this.responseText);
-
-            console.log(objects);
-            Swal.fire({
-                title: "Edit Food-Details",
-                html: '<input id="id" type="hidden" value="' +
-                    objects[`${id}`] + '">' +
-                    '<input id="Food_name" class="swal2-input" placeholder="Food_name" value="' +
-                    objects["Food_name"] + '">' +
-                    '<input id="Price" class="swal2-input" placeholder="Price" value="' +
-                    objects["Price"] + '">' +
-                    '<input style="margin-left:25px;margin-top:20px" id="Image" type="file" class="form-control swa4l1-input mt-4" value="' + objects[`Image`] + '">',
-                preConfirm: () => {
-                    UserEdit_food(id);
-                    xhttp.onreadystatechange = function() {
-                        if (this.readyState == 4 && this.status == 200) {
-
-                        }
-                    };
-                },
-            });
-
-        }
-    };
-}
-
-function UserEdit_food(id) {
-    const Food_name = document.getElementById("Food_name").value;
-    const Price = document.getElementById("Price").value;
-    const Image = document.getElementById("Image");
-    const file = "assets/images/" + Image.files[0].name;
-
-
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("PUT", `http://localhost:3000/Food/${id}`);
-    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhttp.send(
-        JSON.stringify({
-
-            Image: file,
-            Food_name: Food_name,
-            Price: Price,
-
-        })
-
-    );
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            loadcard_food();
-        }
-    };
-}
-
-
-
-function userDelete_food(id) {
-    console.log(id);
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("DELETE", `http://localhost:3000/Food/${id}`);
-    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
-        type: 'warning',
-        icon: 'warning',
-        showCloseButton: true,
-        showCancelButton: true,
-        focusConfirm: false,
-        confirmButtonText: '<i class="fa fa-thumbs-up"></i> Delete!',
-        confirmButtonAriaLabel: 'Thumbs up, Delete!',
-        cancelButtonText: '<i class="fa fa-thumbs-down"></i>',
-        cancelButtonAriaLabel: 'Thumbs down'
-    }).then((result) => {
-        if (result.value) {
-            xhttp.send(
-                JSON.stringify({
-                    id: id,
-                })
-            );
-            xhttp.onreadystatechange = function() {
-                if (this.readyState == 4) {
-                    swal.fire({
-                        title: "Deleted Successfully",
-                        icon: "success",
-                        confirmButtonText: "OK"
-                    })
-                    loadcard_food();
-                }
-            };
-        }
-    });
 }
